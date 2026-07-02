e23e9764cc53e20692fdd85ed0c83de9b27526b6
Sakshi Chouhan <161092908+ChouhanSak@users.noreply.github.com>
2026-07-02 21:28:53 +0530

    Time: 2 ms (99.36%), Space: 47.3 MB (23.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,24 +1,22 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // 1. Syntax: Creating a HashMap. 
-        // Key will store the 'number', Value will store its 'index'.
+        
         Map<Integer, Integer> map = new HashMap<>();
         
-        // 2. Syntax: A standard 'for' loop to traverse the array
+        
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             
-            // 3. Syntax: map.containsKey() checks if the number exists in our notebook
+
             if (map.containsKey(complement)) {
-                // map.get() retrieves the value (index) of that complement
                 return new int[] { map.get(complement), i };
             }
             
-            // 4. Syntax: map.put() adds the current number and its index to the notebook
+        
             map.put(nums[i], i);
         }
         
-        // Return an empty array if no solution is found (though constraints say one always exists)
+     
         return new int[] {};
     }
 }
