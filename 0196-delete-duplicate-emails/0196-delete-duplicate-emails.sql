1a3c8cdd2c9fef382d19d3fff859e3adcb14633d
Sakshi Chouhan <161092908+ChouhanSak@users.noreply.github.com>
2026-07-02 16:51:00 +0530

    Time: 567 ms (95.24%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,7 +1,9 @@
-# Write your MySQL query statement below
-#find min[id] for each email
-DELETE P1
-FROM Person AS P1
-JOIN Person AS P2
-ON P1.email=P2.email
-AND P1.id>P2.id
+DELETE FROM Person
+WHERE id NOT IN (
+    SELECT id
+    FROM (
+        SELECT MIN(id) AS id
+        FROM Person
+        GROUP BY email
+    ) AS temp
+);
