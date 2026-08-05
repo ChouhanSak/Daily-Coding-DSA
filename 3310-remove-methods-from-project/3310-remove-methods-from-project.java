2683c94349282b906c7c02c44da0876d1f8278dc
Sakshi Chouhan <161092908+ChouhanSak@users.noreply.github.com>
2026-08-05 23:24:40 +0530

    Time: 47 ms (97.12%), Space: 274.8 MB (93.27%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] inv : invocations) {
+            adj.get(inv[0]).add(inv[1]);
+        }
+
+       
+        boolean[] suspicious = new boolean[n];
+        suspicious[k] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(k);
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            for (int neighbor : adj.get(node)) {
+                if (!suspicious[neighbor]) {
+                    suspicious[neighbor] = true;
+                    queue.offer(neighbor);
+                }
+            }
+        }
+
+        
+        for (int[] inv : invocations) {
+            int u = inv[0];
+            int v = inv[1];
+            if (!suspicious[u] && suspicious[v]) {
+               
+                List<Integer> allMethods = new ArrayList<>();
+                for (int i = 0; i < n; i++) {
+                    allMethods.add(i);
+                }
+                return allMethods;
+            }
+        }
+
+        
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
